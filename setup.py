d1647dcc454a0beedda9209bedf24869c9f33ab6
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2022-02-09 16:40:15 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+f=open('README.rst', 'r')
+readme=f.read()
+f.close()
+
+
+setup(
+    name='CalCalc',
+    version='0.0.1',
+    description='CalCalc: some catchy description',
+    long_description=readme,
+    author='Christian H. Bye',
+    author_email='chbye@berkeley.edu',
+    url='https://github.com/christianhbye/calcalc',
+    packages=['calcalc'],
+    include_package_data=True,
+    python_requires='>=3.6',
+    license='MIT',
+    classifiers=[
+               'Intended Audience :: End Users/Desktop',
+               'License :: OSI Approved :: MIT License',
+               'Programming Language :: Python :: 3',
+               'Topic :: Utilities'
+    ],
+)
