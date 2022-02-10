d5999ad1b1a81f7320f6bf9741d2f7b4ce545ea5
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2022-02-10 08:39:03 -0800

    blacken setup.py

--- setup.py
+++ setup.py
@@ -3,27 +3,27 @@ try:
 except ImportError:
     from distutils.core import setup
 
-f=open('README.rst', 'r')
-readme=f.read()
+f = open("README.rst", "r")
+readme = f.read()
 f.close()
 
 
 setup(
-    name='CalCalc',
-    version='0.0.1',
-    description='CalCalc: some catchy description',
+    name="CalCalc",
+    version="0.0.1",
+    description="CalCalc: some catchy description",
     long_description=readme,
-    author='Christian H. Bye',
-    author_email='chbye@berkeley.edu',
-    url='https://github.com/christianhbye/calcalc',
-    packages=['calcalc'],
+    author="Christian H. Bye",
+    author_email="chbye@berkeley.edu",
+    url="https://github.com/christianhbye/calcalc",
+    packages=["calcalc"],
     include_package_data=True,
-    python_requires='>=3.6',
-    license='MIT',
+    python_requires=">=3.6",
+    license="MIT",
     classifiers=[
-               'Intended Audience :: End Users/Desktop',
-               'License :: OSI Approved :: MIT License',
-               'Programming Language :: Python :: 3',
-               'Topic :: Utilities'
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
     ],
 )
