c451838f8e35640bb554723410f82c04c166c3b1
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2022-02-10 07:51:47 -0800

    add w flag, add description to args

--- src/CalCalc.py
+++ src/CalCalc.py
@@ -3,7 +3,18 @@ import numpy as np
 import pytest
 
 parser = ArgumentParser(description="")
-parser.add_argument("Expression", metavar="E", type=str)
+parser.add_argument(
+        "Expression",
+        type=str,
+        help='Input string to calculator. Either to be evaluated by the '
+        'calculator (default) or to be requested to WolframAlpha.'
+        )
+parser.add_argument(
+        '-w',
+        type=bool,
+        default=False,
+        help='Send input to WolframAlpha or not. Default is false'
+        )
 args = parser.parse_args()
 
 
