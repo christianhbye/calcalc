bf7028b9493deb0106fb15e9d3271306ca1546a1
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2022-02-10 20:46:38 -0800

    style changes reported by flake8 and black

--- src/CalCalc.py
+++ src/CalCalc.py
@@ -1,4 +1,4 @@
-import numpy as np # type: ignore
+import numpy as np  # type: ignore
 import requests
 from typing import Union
 import urllib.parse
@@ -12,16 +12,16 @@ def _calculate(expression: str, decimal_precision: int) -> Union[int, float]:
     expression: str, the mathematical expression to evaluate
     decimal_precision: int, digits after the decimal point. Negative values
     rounds the anwser
-    
+
     Returns:
     A number (float or int), the output of the calculation
     """
     return np.around(eval(expression), decimals=decimal_precision)
 
-def _ask_wolfram(expression: str, decimal_precision: int) -> Union[
-        int,
-        float,
-        str]:
+
+def _ask_wolfram(
+    expression: str, decimal_precision: int
+) -> Union[int, float, str]:
     """
     Use Wolfram Alpha to answer the question of the string.
 
@@ -33,39 +33,40 @@ def _ask_wolfram(expression: str, decimal_precision: int) -> Union[
     returns:
     int or float, the numerical answer to the question
     """
-    APP_ID='GETY26-72YYRUHXPV'
-    url_exp=urllib.parse.quote_plus(expression)  # convert to url format
-    url=f'http://api.wolframalpha.com/v2/query?input={url_exp}&appid={APP_ID}'\
-            f'&format=plaintext&output=json'
+    APP_ID = "GETY26-72YYRUHXPV"
+    url_exp = urllib.parse.quote_plus(expression)  # convert to url format
+    url = (
+        f"http://api.wolframalpha.com/v2/query?input={url_exp}&appid={APP_ID}"
+        f"&format=plaintext&output=json"
+    )
     r = requests.get(url).json()
-    d = r['queryresult']['pods'][1]['subpods'][0]['plaintext']
+    d = r["queryresult"]["pods"][1]["subpods"][0]["plaintext"]
     try:  # try convert to int/float
         d = d.split()[0]  # try to discard units
-        if d[-3:] == '...': # get rid of dots in infinite decimals
+        if d[-3:] == "...":  # get rid of dots in infinite decimals
             d = d[:-3]
         d_out = _calculate(d, decimal_precision=decimal_precision)
-    except(SyntaxError, TypeError):
-        print(f'Unable to round to {decimal_precision} decimals')
+    except (SyntaxError, TypeError):
+        print(f"Unable to round to {decimal_precision} decimals")
         d_out = d  # just keep the string
     return d_out
 
+
 def calculate(
-        expression: str,
-        decimal_precision: int = 5,
-        wolfram: bool = False
-        ) -> Union[int, float, str]:
+    expression: str, decimal_precision: int = 5, wolfram: bool = False
+) -> Union[int, float, str]:
     """
     Evaluate a mathematical expresison or answer a question to wolfram
-    
+
     Params:
     expression: str, the mathematical expression to evaluate OR a question
     to be answered by Wolfram Alpha
     decimal_precision: int, digits after the decimal point. Negative values
     rounds the anwser
     wolfram: bool, whether to ask wolfram alpha the question. Default is False.
-    
+
     Returns:
-    int, float or str, the output of the calculation. Can only be str if the 
+    int, float or str, the output of the calculation. Can only be str if the
     format of the wolfram output is unexpected
     """
     if not wolfram:  # use Python
@@ -76,20 +77,21 @@ def calculate(
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
+
     parser = ArgumentParser(description="")
     parser.add_argument(
-            "Expression",
-            type=str,
-            help="Input string to calculator. Either to be evaluated by the "
-            "calculator (default) or to be requested to WolframAlpha.",
-            )
+        "Expression",
+        type=str,
+        help="Input string to calculator. Either to be evaluated by the "
+        "calculator (default) or to be requested to WolframAlpha.",
+    )
     parser.add_argument(
-            "-decimals",
-            metavar='-d',
-            type=int,
-            default=5,
-            help='Number of digits after decimal point of output'
-            )
+        "-decimals",
+        metavar="-d",
+        type=int,
+        default=5,
+        help="Number of digits after decimal point of output",
+    )
     parser.add_argument(
         "-w",
         type=bool,
