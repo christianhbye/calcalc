bcc96aa7132fa0eb80ba3aedfb37119f7c738453
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2022-02-10 18:29:34 -0800

    move argparse stuff to if block

--- src/CalCalc.py
+++ src/CalCalc.py
@@ -1,21 +1,5 @@
-from argparse import ArgumentParser
 import requests
 from typing import Union
-
-parser = ArgumentParser(description="")
-parser.add_argument(
-    "Expression",
-    type=str,
-    help="Input string to calculator. Either to be evaluated by the "
-    "calculator (default) or to be requested to WolframAlpha.",
-)
-parser.add_argument(
-    "-w",
-    type=bool,
-    default=False,
-    help="Send input to WolframAlpha or not. Default is false",
-)
-args = parser.parse_args()
 
 
 def _calculate(expression: str) -> Union[int, float]:
@@ -34,6 +18,11 @@ def _ask_wolfram(expression: str) -> Union[int, float]:
     """
     Use Wolfram Alpha to answer the question of the string
     """
+    url='http://api.wolframalpha.com/v2/query?input='
+    + expression
+    + '&appid=GETY26-72YYRUHXPV'
+    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    d = r.json()
     return None  # need to add some code here
 
 def calculate(expression: str, wolfram: bool = False) -> Union[int, float]:
@@ -55,5 +44,20 @@ def calculate(expression: str, wolfram: bool = False) -> Union[int, float]:
 
 
 if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description="")
+    parser.add_argument(
+            "Expression",
+            type=str,
+            help="Input string to calculator. Either to be evaluated by the "
+            "calculator (default) or to be requested to WolframAlpha.",
+            )
+    parser.add_argument(
+        "-w",
+        type=bool,
+        default=False,
+        help="Send input to WolframAlpha or not. Default is false",
+    )
+    args = parser.parse_args()
     ans = calculate(args.Expression, args.w)
     print(ans)
